1eb290e09ecbb7eba82fe86a136330f16a05f1fb
Tomasz131894 <cudzich40@gmail.com>
2024-03-24 14:01:34 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp22/Program.cs
@@ -0,0 +1,43 @@
+﻿using ConsoleApp22;
+using System;
+using System.Collections.Generic;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Pobieranie instancji loggera.
+        Singleton logger = Singleton.GetInstance();
+
+        // Rejestrowanie kilku komunikatów.
+        logger.Log("Uruchomienie programu.");
+        Thread.Sleep(2000);
+        logger.Log("Załadowanie komponentów.");
+
+        Menu();
+
+        void Menu()
+        {
+            Console.WriteLine("Wybierz opcje:");
+            Console.WriteLine("1 - Otwórz dziennik aplikacji");
+            Console.WriteLine("2 - Zakończ działanie aplikacji");
+            string wybor = Console.ReadLine();
+            switch (wybor)
+            {
+                case "1":
+                    logger.Log("Wyświetlenie dziennika programu");
+                    logger.PrintLog();
+                    Menu();
+                    break;
+
+                case "2":
+                    Environment.Exit(0);
+                    break;
+
+                default:
+                    Console.WriteLine("Nieprawidłowy wybór!!!!");
+                    Menu();
+                    break;
+            }
+        }     
+    }
+}
---
+++ ConsoleApp22/Singleton.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp22
+{
+    class Singleton
+    {
+        private static Singleton instance;
+        private List<string> logMessages;
+
+        // Prywatny konstruktor zapobiegający tworzeniu instancji klasy z zewnątrz.
+        private Singleton()
+        {
+            logMessages = new List<string>();
+        }
+
+        // Metoda do uzyskania instancji klasy (Singleton).
+        public static Singleton GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+
+        // Metoda do rejestrowania komunikatów.
+        public void Log(string message)
+        {
+            logMessages.Add($"{DateTime.Now}: {message}");
+        }
+
+        // Metoda do pobierania dziennika zarejestrowanych komunikatów.
+        public void PrintLog()
+        {
+            foreach (var message in logMessages)
+            {
+                Console.WriteLine(message);
+            }
+        }
+ 
+    }
+}
